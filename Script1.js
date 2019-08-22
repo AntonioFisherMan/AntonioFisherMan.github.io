48b995eb55e749ee8c16f1ba69e58a85095a99c2
AntonioFisherMan <50146990+AntonioFisherMan@users.noreply.github.com>
2019-08-22 12:06:11 +0300

    query

---
+++ Script1.js
@@ -0,0 +1,13 @@
+
+//$('.box').addClass('.box-color');
+//$('.box').removeClass('.box-color');
+//$('.box').toggleClass('.box-color');
+//$('.box').show('.box-color');
+//$('.box').hide('.box-color');
+
+//$('.box').removeElement('.box-color');
+$('.box').createElement('.box-color');
+
+$('.box').removeElement('.box-color');
+
+
