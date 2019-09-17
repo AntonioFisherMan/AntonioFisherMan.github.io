00e7612638dc8d4f33682f741a40d35a08503a50
AntonioFisherMan <50146990+AntonioFisherMan@users.noreply.github.com>
2019-09-17 15:22:33 +0300

    Add files via upload

---
+++ localhost/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<p>Hello world</p>
+<?php
+require("function.php");
+$name="Вася";
+$age="80";
+echo"<h1>Приветasdasdasd мир</h1>";
+echo "<p>".$name."Ест пирожок</p>";
+echo"</p>Ему$age лет</p>";
+?>
+
+</body>
+</html>
