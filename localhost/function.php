0676607fabd7f47190e0e3b92b644f5a76e72187
AntonioFisherMan <50146990+AntonioFisherMan@users.noreply.github.com>
2019-09-18 19:50:09 +0300

    Update function.php

--- localhost/function.php
+++ localhost/function.php
@@ -1,31 +1,37 @@
 <?php
-define("M_PI",3,14);
-if(empty($a))
-{
-echo"<p>существует</p>";
-}
-else{
-echo"<p>не существует</p>";
-}
-$panel=[
-    'Home'=>'/home',
-    'Blog'=>'/blog',
-    'About us'=>'/about us'
+
+$panel = [
+    'Home' => ['Home1' => '/home', 'Home2' => '/home2'],
+    'About us' => ['About us1' => '/about us1', 'About us2' => '/about us2'],
+    'Main' => ['Main1' => '/main1', 'Main2' => '/main2']
 ];
-echo"<pre>";
-var_dump($panel);
-echo"</pre>";
-foreach ($panel as $key=>$value)
+function getConfig($key)
 {
-    echo $key."+".$value."<br>";
+    if (empty($key)) {
+
+        return null;
+    }
+    $config = [
+        'lang' => 'en',
+        'title' => 'первый проект',
+        'description' => 'Работа'
+    ];
+    if (isset($config[$key])) {
+        return $config[$key];
+    } else {
+        return null;
+    }
 }
 ?>
-<nav class="navigate">
-    <ul>
+<nav >
+    <ul class="navigate">
         <?php
-        foreach ($panel as$key=>$value)
-        {
-            echo"<li><a href='$value'>$key</a></li>";
+        foreach ($panel as $key => $value) {
+            echo "<li>$key<div class='divHover'>";
+            foreach ($value as $key2 => $value2) {
+                echo "<span><a href='$value2'>$key2</a></span>";
+            }
+            echo "</div></li>";
         }
         ?>
     </ul>
