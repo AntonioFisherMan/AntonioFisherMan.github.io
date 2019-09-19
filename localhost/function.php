8a7fed9c16dcabdcfb7d9daa1a68ee6fd5c5033e
AntonioFisherMan <50146990+AntonioFisherMan@users.noreply.github.com>
2019-09-19 20:54:17 +0300

    Update function.php

--- localhost/function.php
+++ localhost/function.php
@@ -16,7 +16,7 @@ function getConfig($key)
         'title' => 'первый проект',
         'description' => 'Работа'
     ];
-    if (isset($config[$key])) {
+    if (isset($config[$key])){
         return $config[$key];
     } else {
         return null;
@@ -34,8 +34,80 @@ if($connect)
         return mysqli_fetch_all($result,MYSQLI_ASSOC);
     }
 }
+}
+function getOptionId($name,$value)
+{
+    if(empty($name)||empty($value))
+    {
+        return null;
+    }
+
+    $connect=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    if($connect)
+    {
+        $query="SELECT id FROM options WHERE name='$name' AND VALUE ='$value'";
+        $result=mysqli_query($connect,$query);
+        if($result->num_rows>0){
+            mysqli_close($connect);
+            return mysqli_fetch_assoc($result)['id'];
+        }
+    }
+}
+function getOption($name)
+{
+if(empty($name))
+{
+    return null;
+}
+
+    $connect=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    if($connect)
+    {
+        $query="SELECT name FROM options WHERE name='$name'";
+        $result=mysqli_query($connect,$query);
+        if($result->num_rows>0){
+            mysqli_close($connect);
+            return mysqli_fetch_assoc($result)['name'];
+        }
+    }
+}
+function addOption($name,$value)
+{
+    $connect=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    if($connect)
+    {
+        $query = "INSERT INTO options(name,value) VALUES ('$name','$value')";
+        mysqli_query($connect,$query);
+        if(mysqli_affected_rows($connect)==1){
+
+            return true;
+        }
+        else{
+            return false;
+        }
+        mysqli_close($connect);
+    }
+}
+function deleteOptions($id)
+{
+    if(empty($id))return null;
+    $connect=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    if($connect)
+    {
+        $query="DELETE FROM options WHERE id='$id' LIMIT 1";
+        mysqli_query($connect,$query);
+        if(mysqli_affected_rows($connect)==1){
+
+            return true;
+        }
+        else{
+            return false;
+        }
+        mysqli_close($connect);
+    }
 
 }
+
 ?>
 <nav >
     <ul class="navigate">
